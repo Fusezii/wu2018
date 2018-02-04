3786262743c0d50804e7996f0f21053c6f03ee8c
NEXT-LDD20GTL6M\NEXT Speed <fuse.254069@gmail.com>
2018-02-04 09:51:00 +0700

    update

--- tests/AITest.php
+++ tests/AITest.php
@@ -4,10 +4,17 @@ use PHPUnit\Framework\TestCase;
 
 final class AITest extends TestCase
 {
+    public function testGender_Female(): void
+    {
+        $result = AI::getGender('สวัสดีค่ะ');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
     public function testGender_Male(): void
     {
         $result = AI::getGender('สวัสดีครับ');
-        $expected_result = 'Male';
+        $expected_result = 'male';
         $this->assertEquals($expected_result, $result);
     }
+    
 }
